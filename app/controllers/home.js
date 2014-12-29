b2ae34b485fc829176785d4542e913c411a6ae09
igorzg <igor.zg1987@gmail.com>
2014-12-29 14:57:47 +0100

    Updated logger docs added github buttons

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,10 +7,50 @@ var di = require('mvcjs'), // mvcjs as node package
 
 
 HomeController = CoreController.inherit({}, {
+    /**
+     * action index
+     * @param params
+     * @param data
+     * @returns {*|string}
+     * @constructor
+     */
     action_index: function Core_create(params, data) {
+        this.locals.scripts.push({
+            src: 'https://buttons.github.io/buttons.js',
+            id: 'github-bjs',
+            async: true,
+            defer: true
+        });
         return this.renderFile('home/index', this.locals);
     },
 
+    /**
+     * Before each action
+     * @returns {*}
+     */
+    beforeEach: function () {
+        var _super = this._super();
+        this.locals.scripts.push({
+            src: '/assets/js/prism.js'
+        });
+        return _super;
+    },
+    /**
+     * Action logger
+     * @param params
+     * @param data
+     * @returns {*|string}
+     * @constructor
+     */
+    action_logger: function HomeController_content(params, data) {
+        return this.renderFile('home/logger', this.locals);
+    },
+    /**
+     * Before content
+     * @param params
+     * @param data
+     * @returns {Promise}
+     */
     before_content: function (params, data) {
         var pathName = this._request.parsedUrl.pathname;
         return new Promise(function(resolve, reject) {
@@ -23,13 +63,15 @@ HomeController = CoreController.inherit({}, {
             });
         });
     },
-    action_logger: function HomeController_content(params, data) {
-        return this.renderFile('home/logger', this.locals);
-    },
+    /**
+     * Action content
+     * @param params
+     * @param data
+     * @returns {*|string}
+     * @constructor
+     */
     action_content: function HomeController_content(params, data) {
-        this.locals.scripts.push({
-            src: '/assets/js/prism.js'
-        });
+
         this.locals.content = data.text;
         return this.renderFile('home/content', this.locals);
     }

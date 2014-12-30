f5644bbb15c7c40fad0ea90d525931b915b669da
igorzg <igor.zg1987@gmail.com>
2014-12-30 11:53:22 +0100

    Update of controllers doc

--- env/dev/import.js
+++ env/dev/import.js
@@ -19,10 +19,11 @@ module.exports = function (componet, di) {
     var content = JSON.parse(di.readFileSync('@{envPath}/imports/content.json'));
     contentModel.importData(content);
 
-
-    //var util = require('util');
-    //var article = util.inspect(di.readFileSync('@{basePath}/coverage/article.html'));
-    //article = article.replace(/\\\'/g, "'");
-    //article = article.replace(/"/g, '\\"');
-    //console.log(article);
+    /*
+    var util = require('util');
+    var article = util.inspect(di.readFileSync('@{basePath}/coverage/article.html'));
+    article = article.replace(/\\\'/g, "'");
+    article = article.replace(/"/g, '\\"');
+    console.log(article);
+    */
 };
